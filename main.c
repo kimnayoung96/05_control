f142ec5a85a1541e33961eff7b9c6af23042a8ef
kimnayoung96 <55428158+kimnayoung96@users.noreply.github.com>
2019-10-08 15:30:34 +0900

    02

--- main.c
+++ main.c
@@ -5,17 +5,10 @@
 
 int main(int argc, char *argv[]) {
 	
-	int i=0, meter;
+	int i;
 	
-	while (i<3)
-	 {
-	 	meter = i*1609;
-	 	printf("%d 마일은 %d 미터입니다.\n", i, meter);
-	 	i++;
-	 	
-	 }
-	
-	
+	for(i=0; i<10; i++)
+	 printf("Hello World!\n");
 		
 	return 0;
 }
