9ef8a4a439d28525a021d19fd1cde0c54dffc33a
kimnayoung96 <55428158+kimnayoung96@users.noreply.github.com>
2019-10-08 16:11:06 +0900

    03

--- main.c
+++ main.c
@@ -5,10 +5,20 @@
 
 int main(int argc, char *argv[]) {
 	
-	int i;
+	int num = 0;
+	char c;
 	
-	for(i=0; i<10; i++)
-	 printf("Hello World!\n");
-		
+	printf("input a string : ");
+	
+	while ((c = getchar()) != '\n')
+	{ 
+		if (c <= '9' && c>= '0')
+	    {
+	    	num++;
+	   	}	
+	}
+	
+	printf("%i\n", num);
+	
 	return 0;
 }
